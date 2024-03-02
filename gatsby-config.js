c80338e56a417f4c1377f5e470ce7ff4984070f3
araaancode <araaancode@gmail.com>
2024-03-02 15:43:17 +0330

    litte change

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `https://cdn-icons-png.flaticon.com/128/523/523495.png`, // This path is relative to the root of the site.
+        icon: `src/images/me.jpg`, // This path is relative to the root of the site.
       },
     },
   ],
